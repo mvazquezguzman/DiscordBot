79907a953a68c22e0724e05cf40376424393e1d8
Abdou Senghore <156225397+aSenghore@users.noreply.github.com>
2025-03-04 15:47:30 -0500

    Update Footer.js

--- src/frontend/src/components/Footer.js
+++ src/frontend/src/components/Footer.js
@@ -5,10 +5,10 @@ import { Typography } from '@mui/material';
 const Footer = () => {
     return (
         <footer style={{ textAlign: 'center', marginTop: '20px', padding: '10px', fontSize: '0.9em', color: '#777' }}>
-            <Typography variant="body2">
+            <Typography variant="body2" class='footerColor'>
                 © 2024 BashBot. All rights reserved.
             </Typography>
-            <Typography variant="body2">
+            <Typography variant="body2" class='footerColor'>
                 This project is licensed under the <a href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer">MIT License</a>.
             </Typography>
         </footer>
