be0cc229b7c7e23fc3b4c00a0d41e5b7095bf892
Abdou Senghore <156225397+aSenghore@users.noreply.github.com>
2025-04-30 23:46:55 -0400

    Update Footer.js

--- src/frontend/src/components/Footer.js
+++ src/frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <footer style={{ textAlign: 'center', marginTop: '20px', padding: '10px', fontSize: '0.9em', color: '#777' }}>
             <Typography variant="body2" class='footerColor'>
-                © 2024 BashBot. All rights reserved.
+                © 2024 CtrlAltKick. All rights reserved.
             </Typography>
             <Typography variant="body2" class='footerColor'>
                 This project is licensed under the <a href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer">MIT License</a>.
